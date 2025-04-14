c974d39d232b8df0406dbad1de660141cb5c244f
Vishrut Vats <vishrut.vats.developer@gmail.com>
2025-04-14 06:06:26 +0530

    redirect commented code

--- next-js-practice/next.config.ts
+++ next-js-practice/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here. For understanding It has been edited. */
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/blog/first",
+  //       permanent: true,
+  //     },
+  //   ];
+  // }
 };
 
 export default nextConfig;
